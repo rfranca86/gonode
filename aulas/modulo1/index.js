308035ae3c0e121e5155bdc10f8bfea87afacdd6
ricardofranca <ricardofranca@ext.cvc.com.br>
2019-02-26 18:40:25 -0300

    gonode modulo1

--- aulas/modulo1/index.js
+++ aulas/modulo1/index.js
@@ -1,26 +1,24 @@
 const express = require("express");
+const nunjucks = require('nunjucks');
 
 const app = express();
 
-const logMiddleware = (req, res, next) => {
-  console.log(
-    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
-  );
-
-  req.appName = "GoNode";
-  return next();
-};
-
-app.use(logMiddleware);
-
-app.get("/", (req, res) => {
-  return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+  watch: true,
 });
 
-app.get("/nome/:name", (req, res) => {
-  return res.json({
-    message: `Welcome, ${req.params.name}`
-  });
+app.set('view engine', 'njk');
+
+const users = ['Foo', 'Fulano', 'Bar'];
+
+app.get("/", (req, res) => {
+  return res.render('list', { users });
+});
+
+app.get('/new', (req, res) => {
+  return res.render('new');
 });
 
 app.listen(3000);
