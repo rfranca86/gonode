be3507469ef54481e0646a8655d1e0a66cea856c
rfranca86 <universius@gmail.com>
2019-02-26 10:09:33 -0300

    gonode start

---
+++ aulas/modulo1/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const app = express();
+
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host} | URL: ${req.url} | METHOD: ${req.method}`
+  );
+
+  req.appName = "GoNode";
+  return next();
+};
+
+app.use(logMiddleware);
+
+app.get("/", (req, res) => {
+  return res.send(`Bem-vindo ao ${req.appName}, ${req.query.name}`);
+});
+
+app.get("/nome/:name", (req, res) => {
+  return res.json({
+    message: `Welcome, ${req.params.name}`
+  });
+});
+
+app.listen(3000);
